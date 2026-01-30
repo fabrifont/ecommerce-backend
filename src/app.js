0cf29c779214a5278ee4c6d1aa7f10d82bb69078
Fabrizio Fontanarrosa <fabriziofontanarrosa@gmail.com>
2026-01-29 23:55:38 -0300

    modify directory structure for project

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import { ProductManager, CartManager } from "./classes.mjs";
+import express from "express";
+import handlebars from "express-handlebars";
+import productsRouter from "./productsRouter";
+const __dirname = import.meta.dirname;
+
+const app = express();
+const PORT = 8080;
+app.use(express.json());
+app.use(express.static("public"));
+app.set("view engine", "handlebars");
+app.engine("handlebars", handlebars.engine());
+
+// ----------------------------  API: Manejo de productos  ---------------------------------------
+
+const productManager = new ProductManager();
+productManager.load("./products.json");
+app.use("/products", productsRouter(productManager));
+
+// ----------------------------  API: Manejo de carritos  ---------------------------------------
+
+const cartManager = new CartManager();
+cartManager.load("./carts.json");
+app.use("/carts", cartsRouter(cartManager));
+
+// ----------------------------  Server Side Rendering (Handlebars)  ---------------------------------------
+
+app.get("/", (req, res) => {
+	res.render("index", {
+		layout: "main.handlebars",
+		products: productManager.products,
+	});
+});
+
+// ---------------------------------------------------------------------------------------------------------
+
+app.listen(PORT, () => {
+	console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
